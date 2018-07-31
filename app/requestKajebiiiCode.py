447373e4697a08734e0d144b63dc5e2c4fe982c3
Baekjoon Choi <choi@startlink.io>
2018-07-31 23:17:56 +0900

    채점 현황 주소: /status/ -> /status 로 바뀜

--- app/requestKajebiiiCode.py
+++ app/requestKajebiiiCode.py
@@ -43,7 +43,7 @@ def downloadCode(submit_id, problem):
 	imagedir = 'app/data/private/code/' + problem + '/' + submit_id + '.png'
 
 def findAClist(user_id, top_submit):
-	urlData = safeData(isPost=False, url='https://www.acmicpc.net/status/?user_id=%s&result_id=4&top=%s' % (user_id, top_submit));
+	urlData = safeData(isPost=False, url='https://www.acmicpc.net/status?user_id=%s&result_id=4&top=%s' % (user_id, top_submit));
 	htmlData = urlData.content.decode('utf-8');
 
 	result = re.findall('<tr ([\s\S]*?)</tr>', htmlData, re.DOTALL)
