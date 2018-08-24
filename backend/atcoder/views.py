511fb53fabe5e86dee36cdb5df7cc95a2d5ace74
kajebiii <kajebiii@gmail.com>
2018-08-24 10:00:01 +0900

    add atcoder_base_information

--- backend/atcoder/views.py
+++ backend/atcoder/views.py
@@ -1,12 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from .apps import atcoder, lock
 import requests
 import json
 
 
 @api_view(['GET'])
-def getProblemList(request):
+def get_problem_list(request):
     atcoder_id = request.data.get('atcoder_id')
     if atcoder_id is None:
         return Response("'atcoder_id' field required.", status=status.HTTP_400_BAD_REQUEST)
@@ -24,3 +25,12 @@ def getProblemList(request):
         if sub['result'] == "AC" or not sub['problem_id'] in sub_info:
             sub_info[sub['problem_id']] = sub['result']
     return Response(sub_info)
+
+
+@api_view(['GET'])
+def get_base_information(request):
+    lock.acquire()
+    base_information = atcoder
+    lock.release()
+    return Response(base_information)
+
