3a922532260d988ca5cd0630d64e7261868cc480
kajebiii <kajebiii@gmail.com>
2018-08-26 23:19:32 +0900

    add blank HandleLoginPage

--- frontend/src/components/pages/HandleLoginPage/index.js
+++ frontend/src/components/pages/HandleLoginPage/index.js
@@ -4,6 +4,7 @@ import { PageTemplate } from 'components'
 import { Input, Button } from 'components'
 
 const HandleLoginPage = ({ handle_state, children, ...props }) => {
+  console.log(handle_state)
   return (
     <PageTemplate {...props}>
       {children}
--- frontend/src/containers/HandleLoginPage.js
+++ frontend/src/containers/HandleLoginPage.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { HandleLoginPage } from 'components'
 
-const mapStateToProps = (state) => ({handle_state: state.user.handle_state})
+const mapStateToProps = (state) => ({handle_state: state.users.handle_state})
 const mapDispatchToProps = (dispatch) => ({})
 export default connect(mapStateToProps, mapDispatchToProps)((props)=>(<HandleLoginPage {...props} />))
