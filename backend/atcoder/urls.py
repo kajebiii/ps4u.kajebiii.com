83d0708f6fec95ec4e1ba75ccdbb0bccf6d06fc2
kajebiii <kajebiii@gmail.com>
2018-08-28 11:46:35 +0900

    url prefix atcoder to backend urls

--- backend/atcoder/urls.py
+++ backend/atcoder/urls.py
@@ -3,8 +3,8 @@ from atcoder import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^atcoder/problem-list/$', views.get_problem_list),
-    url(r'^atcoder/base-information/$', views.get_base_information),
+    url(r'^problem-list/$', views.get_problem_list),
+    url(r'^base-information/$', views.get_base_information),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
