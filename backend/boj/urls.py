31f88cb745ea6ac56c79a6c69a1bbf5587099c4a
kajebiii <kajebiii@gmail.com>
2018-08-28 20:01:21 +0900

    add boj app

---
+++ backend/boj/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.urlpatterns import format_suffix_patterns
+from kajebiii_boj import views
+from django.conf.urls import url
+
+urlpatterns = [
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
