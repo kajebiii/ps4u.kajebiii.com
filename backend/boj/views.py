0aba20cb1bc2239cb92e990233694c8af6b9c3c2
kajebiii <kajebiii@gmail.com>
2018-08-28 22:07:35 +0900

    implement get_ac_problem_list

--- backend/boj/views.py
+++ backend/boj/views.py
@@ -21,3 +21,17 @@ def get_problem_list(request, user_id):
         for i in range(0, len(wa_problems), 2):
             problem_list[int(wa_problems[i])] = "WA"
     return Response(problem_list)
+
+
+@api_view(['GET'])
+def get_ac_problem_list(request, user_id):
+    urlData = safeData(isPost=False, url='https://www.acmicpc.net/user/%s' % user_id)
+    htmlData = urlData.content.decode('utf-8')
+
+    tables = re.findall('<div class="panel panel-default">([\s\S]*?)</div>[\s]*?</div>', htmlData, re.DOTALL)
+    problem_list = []
+    if len(tables) == 2:
+        ac_table = tables[0]
+        ac_problems = re.findall('class="result-ac">([\s\S]*?)</a>', ac_table, re.DOTALL)
+        problem_list = [int(ac_problems[i]) for i in range(0, len(ac_problems), 2)]
+    return Response(problem_list)
