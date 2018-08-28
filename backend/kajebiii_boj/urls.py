15b8519324817c35631bac9b0b39bed85be3439f
kajebiii <kajebiii@gmail.com>
2018-08-28 19:01:46 +0900

    add last-ac-source url in backend

--- backend/kajebiii_boj/urls.py
+++ backend/kajebiii_boj/urls.py
@@ -3,6 +3,7 @@ from kajebiii_boj import views
 from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^last-ac-source/(?P<problem>[0-9]+)/$', views.get_last_ac_source),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/kajebiii_boj/views.py
+++ backend/kajebiii_boj/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Submission
+from django.db.models import Q
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_last_ac_source(request, problem):
+    last_ac_submission = Submission.objects.filter(Q(problem=problem) & Q(result="ac")).last()
+    if last_ac_submission is None:
+        return Response("No AC Submission")
+    return Response(last_ac_submission.source)
