0ec7c4aebcaa5e0e259f61ae38d9fda045119153
kajebiii <kajebiii@gmail.com>
2018-08-30 20:26:25 +0900

    add boj apps (synchronize category contest problem data)

--- backend/boj/admin.py
+++ backend/boj/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Contest, Problem
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Contest)
+admin.site.register(Problem)
+
