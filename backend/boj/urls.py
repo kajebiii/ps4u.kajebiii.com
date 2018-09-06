e1cfb40b4d41474eaa3bdac83da712ada60c2513
kajebiii <kajebiii@gmail.com>
2018-09-06 12:33:17 +0900

    add get_problems_sort_by_description_length

--- backend/boj/urls.py
+++ backend/boj/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('problem-list/<slug:user_id>/', views.get_problem_list),
     path('ac-problem-list/<slug:user_id>/', views.get_ac_problem_list),
     path('all-contest-with-problem/', views.get_all_contest_with_problem),
+    path('all_problems_sort_by_description_length/', views.get_problems_sort_by_description_length),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
