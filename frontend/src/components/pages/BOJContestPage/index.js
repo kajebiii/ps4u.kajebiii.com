797446bb0f573f4f906a6a4d4d62685469f9874e
kajebiii <kajebiii@gmail.com>
2018-09-06 22:23:08 +0900

    change table to a tags in BOJContestPage

--- frontend/src/components/pages/BOJContestPage/index.js
+++ frontend/src/components/pages/BOJContestPage/index.js
@@ -8,11 +8,15 @@ const Wrapper = styled.div`
 font-family: ${font('primary')};
 color: ${palette('grayscale', 0)};
 
-.bojtable td.AC{
-	background-color: #dff0d8;
+.boj_problems a.NONE{
+  color: #0076C0;
 }
-.bojtable td.WA{
-	background-color: #fcf8e3;
+.boj_problems a.AC{
+  color: #009F6B;
+  font-weight: bold;
+}
+.boj_problems a.WA{
+	color: #e74c3c;
 }
 `
 
@@ -47,30 +51,32 @@ class BOJContestPage extends React.Component {
         type="checkbox" id="show_can_not_all_solve" 
         checked={this.state.show_can_not_all_solve} onChange={this.handleInputChange}>
       </input>
+      <Wrapper><div className="boj_problems">
       {
         contests_with_problem_state.map( (contest) => {
           const { id, title, parent_title, parent_id, problems } = contest
-          return <Wrapper key={id}>
+          return <div key={id}>
             <div className="d-flex justify-content-between">
               <h5><a href={"https://acmicpc.net/category/detail/"+id}>{title}</a></h5>
               <p className="font-weight-light"><a href={"https://acmicpc.net/category/"+parent_id}>{parent_title}</a></p>
             </div>
-            <table className="bojtable table table-sm table-bordered text-center"><tbody><tr>
+            <div className="d-flex justify-content-between">
             {
               problems.map( (problem) => {
-                if(problem in user_state) {
-                  const result = user_state[problem]
-                  if(result === "AC")
-                    return <td className="AC" key={problem}><a href={"https://acmicpc.net/category/problem/"+problem}>{problem}</a></td> 
-                  return <td className="WA" key={problem}><a href={"https://acmicpc.net/category/problem/"+problem}>{problem}</a></td> 
-                }
-                return <td key={problem}><a href={"https://acmicpc.net/category/problem/"+problem}>{problem}</a></td>
+                return (
+                  <a key={problem} href={"https://acmicpc.net/problem/"+problem}
+                     className={problem in user_state ? (user_state[problem] === "AC" ? "AC" : "WA") : "NONE"}>
+                    {problem}
+                  </a>
+                )
               })
             }
-            </tr></tbody></table>
-          </Wrapper>
+            </div>
+            <hr/>
+          </div>
         })
       }
+      </div></Wrapper>
       {children}
       </PageTemplate>
     )
