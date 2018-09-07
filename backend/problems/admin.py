caf043d123498931c1e619637a7e12f418f9d8bf
kajebiii <kajebiii@gmail.com>
2018-09-07 11:48:42 +0900

    add problem tags / delete admin.py (b/c of abstract class)

--- backend/problems/admin.py
+++ backend/problems/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Problem
+from .models import Tag
 
-admin.site.register(Problem)
-
+admin.site.register(Tag)
--- backend/problems/models.py
+++ backend/problems/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=99, blank=False)
+
+
 class Problem(models.Model):
     thinking_rating = models.IntegerField(default=-1)
     implement_rating = models.IntegerField(default=-1)
     problem_summary = models.TextField(default="")
     solution_summary = models.TextField(default="")
+    tags = models.ManyToManyField(Tag)
 
     class Meta:
         abstract = True
