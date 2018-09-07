32485f4dadede4e43659b46f7e95cc7fa91c6d0b
kajebiii <kajebiii@gmail.com>
2018-09-07 14:11:39 +0900

    add IsAdminUserOrReadOnly permission to tag views

--- backend/problems/views.py
+++ backend/problems/views.py
@@ -1,15 +1,24 @@
 from rest_framework import generics
+from rest_framework import permissions
 from django.shortcuts import get_object_or_404
 from problems.serializers import *
+
+
+class IsAdminUserOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_staff or request.method in ['GET', 'HEAD', 'OPTIONS']
 
 
 class TagListView(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagListSerializer
+    permission_classes = (IsAdminUserOrReadOnly, )
 
 
 class TagView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TagSerializer
+    permission_classes = (IsAdminUserOrReadOnly, )
 
     def get_object(self):
         tag_id = self.kwargs["tag_id"]
